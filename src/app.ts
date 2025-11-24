5c54dee9e6fda3d1a900782c7984f05b0f0c374f
Jainam <jainammithalaljain@gmail.com>
2025-11-24 16:08:56 +0530

    updated for vercel deployment

--- src/app.ts
+++ src/app.ts
@@ -180,8 +180,5 @@ app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-const port = Number(process.env.PORT || 4000);
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
 
 export default app;
